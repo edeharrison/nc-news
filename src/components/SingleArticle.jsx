e34648d65a53f5caca6ae7e9b94032b40bcb5c6a
Ede Harrison <ede_harrison@Edes-MacBook-Pro.local>
2023-03-14 10:16:15 +0000

    setup SingleArticle component

---
+++ src/components/SingleArticle.jsx
@@ -0,0 +1,17 @@
+export default function SingleArticle({ singleArticle, isLoading }) {
+  //   return isLoading ? (
+  //     <h1>Loading...</h1>
+  //   ) : (
+  //     <main className="single-article">
+  //       <h2>{article.title}</h2>
+  //       <p>{article.author}</p>
+  //       <p>{article.topic}</p>
+  //       <p>{article.created_at}</p>
+  //       <p>{article.votes} likes</p>
+  //       <p>{article.comment_count} comments</p>
+  //       <img src={article.article_img_url} alt="article thumbnail image" />
+  //     </main>
+  //   );
+
+  console.log(singleArticle)
+}
